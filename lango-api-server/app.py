6a12b66b81b2aac7c2beb69b948fac32ca28b246
marcociav <marco.bizzness@gmail.com>
2022-05-07 20:10:10 +0200

    Configured Dockerfile for gcloud deployment

--- lango-api-server/app.py
+++ lango-api-server/app.py
@@ -111,4 +111,4 @@ if __name__ == '__main__':
     log_config = uvicorn.config.LOGGING_CONFIG
     log_config["formatters"]["access"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
     log_config["formatters"]["default"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
-    uvicorn.run(app, host='0.0.0.0', port=5000, log_config=log_config)
+    uvicorn.run(app, host='0.0.0.0', port=8080, log_config=log_config)
